d00325fe9078dbd9ec3010bcf6577718630134e9
Sony MelliFriady <smfriady@gmail.com>
2020-09-11 04:02:43 +0700

    first commit typescriptlang

---
+++ build/typeDasar.js
@@ -0,0 +1,20 @@
+"use strict";
+// number
+var nama = 'sony';
+nama = 'string';
+// number
+var umur = 25;
+umur = 24;
+umur = 20;
+// boolean
+var isJavascipt;
+isJavascipt = true;
+isJavascipt = false;
+// any = data nya bisa apa saja dalam 1 variable
+var heroes = "iron man";
+heroes = 'string';
+heroes = 24; //number
+heroes = []; // array
+heroes = {}; //object
+heroes = true; //boolean
+heroes = false; //boolean
---
+++ src/typeDasar.ts
@@ -0,0 +1,27 @@
+// number
+let nama: string = 'sony';
+
+nama = 'string';
+
+// number
+let umur: number = 25;
+
+umur = 24;
+umur = 20;
+
+// boolean
+let isJavascipt: boolean;
+
+isJavascipt = true;
+isJavascipt = false;
+
+// any = data nya bisa apa saja dalam 1 variable
+let heroes: any = "iron man";
+heroes = 'string';
+heroes = 24; //number
+heroes = []; // array
+heroes = {}; //object
+heroes = true; //boolean
+heroes = false; //boolean
+
+
