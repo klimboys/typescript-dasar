7e8e49bb61ce827f1dee2235e4c50b0bf1df5b40
Sony MelliFriady <smfriady@gmail.com>
2020-09-11 04:13:37 +0700

    tipe data array

---
+++ build/array.js
@@ -0,0 +1,12 @@
+"use strict";
+// arayy
+// let array:number [] = [1, 2, 3];
+var array;
+// akan error karena jelas variable di atas number
+// 1. array = ['string']
+array = [1, 2, 3];
+// string
+var array2;
+array2 = ['oke', 'ok'];
+var array3;
+array3 = [1, 'oke', true, {}];
---
+++ src/array.ts
@@ -0,0 +1,15 @@
+// arayy
+// let array:number [] = [1, 2, 3];
+let array: number[];
+// akan error karena jelas variable di atas number
+// 1. array = ['string']
+array = [1, 2, 3];
+
+// string
+let array2: string[];
+
+array2 = ['oke', 'ok'];
+
+let array3: any[];
+
+array3 = [1, 'oke', true, {}];
