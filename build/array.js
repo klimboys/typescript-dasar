08fce6e7970564a90872ee8edf4a115c30bfc0e4
Sony MelliFriady <smfriady@gmail.com>
2020-09-11 04:19:14 +0700

    tipe data array tuples

--- build/array.js
+++ build/array.js
@@ -10,3 +10,10 @@ var array2;
 array2 = ['oke', 'ok'];
 var array3;
 array3 = [1, 'oke', true, {}];
+// tuples
+// adalah sebuah array yang berisi berbagaimacam data tetapi nilai atau item nya terbatas
+var biodata;
+biodata = ['Tasik Malaya', 14];
+// biodata = [12, 'Bandung']; salah
+// biodata = ['Bandung', true] salah
+// biodata = ['Bandung', 14, true] salah
--- src/array.ts
+++ src/array.ts
@@ -13,3 +13,13 @@ array2 = ['oke', 'ok'];
 let array3: any[];
 
 array3 = [1, 'oke', true, {}];
+
+// tuples
+// adalah sebuah array yang berisi berbagaimacam data tetapi nilai atau item nya terbatas
+
+let biodata: [string, number];
+biodata = ['Tasik Malaya', 14];
+// biodata = [12, 'Bandung']; salah
+// biodata = ['Bandung', true] salah
+// biodata = ['Bandung', 14, true] salah
+
